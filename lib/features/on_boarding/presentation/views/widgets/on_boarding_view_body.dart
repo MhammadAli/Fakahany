7789ed21752ed1dd735879f169a7ac67eee08578
MhammadAli <Mohammed180726@feng.bu.edu.eg>
2024-11-18 16:50:34 +0200

    Adding Dots indicators.

--- lib/features/on_boarding/presentation/views/widgets/on_boarding_view_body.dart
+++ lib/features/on_boarding/presentation/views/widgets/on_boarding_view_body.dart
@@ -1,5 +1,7 @@
+import 'package:dots_indicator/dots_indicator.dart';
 import 'package:flutter/material.dart';
 
+import '../../../../../utils/app_colors.dart';
 import 'on_boarding_page_view.dart';
 
 class OnBoardingViewBody extends StatelessWidget {
@@ -10,7 +12,14 @@ class OnBoardingViewBody extends StatelessWidget {
     return Column(
       children: [
         Expanded(
-          child: SafeArea(child: OnBoardingPageView()),
+          child: OnBoardingPageView(),
+        ),
+        DotsIndicator(
+          dotsCount: 2,
+          decorator: DotsDecorator(
+            activeColor: AppColors.primaryColor,
+            color: AppColors.primaryColor.withOpacity(0.5),
+          ),
         ),
       ],
     );
