8829d1b55f2d881154260e8f24abdd5ccc21adb1
MhammadAli <Mohammed180726@feng.bu.edu.eg>
2024-11-18 19:40:36 +0200

    Adding Custom Button.

---
+++ lib/constants.dart
@@ -0,0 +1,4 @@
+
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+
+final kHorizontalPadding = 16.0.w;
--- lib/features/on_boarding/presentation/views/widgets/on_boarding_view_body.dart
+++ lib/features/on_boarding/presentation/views/widgets/on_boarding_view_body.dart
@@ -1,5 +1,8 @@
 import 'package:dots_indicator/dots_indicator.dart';
+import 'package:fakahany/constants.dart';
+import 'package:fakahany/core/widgets/custom_button.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 import '../../../../../utils/app_colors.dart';
 import 'on_boarding_page_view.dart';
@@ -20,6 +23,21 @@ class OnBoardingViewBody extends StatelessWidget {
             activeColor: AppColors.primaryColor,
             color: AppColors.primaryColor.withOpacity(0.5),
           ),
+        ),
+        SizedBox(
+          height: 29.h,
+        ),
+        Padding(
+          padding: EdgeInsets.symmetric(
+            horizontal: kHorizontalPadding,
+          ),
+          child: CustomButton(
+            onPressed: () {},
+            text: 'ابدأ الآن',
+          ),
+        ),
+        SizedBox(
+          height: 30.h,
         ),
       ],
     );
