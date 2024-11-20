faf2ad525f0bf56f5decdab5cfc5832bb12e8f8d
MhammadAli <Mohammed180726@feng.bu.edu.eg>
2024-11-20 20:24:30 +0200

    Adding dartz package.

--- lib/features/auth/domain/repos/auth_repo.dart
+++ lib/features/auth/domain/repos/auth_repo.dart
@@ -1,7 +1,9 @@
+import 'package:dartz/dartz.dart';
+import 'package:fakahany/core/errors/failures.dart';
 import 'package:fakahany/features/auth/domain/entities/user_entity.dart';
 
 abstract class AuthRepo {
-  Future<UserEntity> createUserWithEmailAndPassword(
+  Future<Either<Failure,UserEntity>> createUserWithEmailAndPassword(
     String email,
     String password,
   );
