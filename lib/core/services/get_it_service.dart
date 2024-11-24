ac6906458f40a2c910900ae41c6751aac7f16a2b
MhammadAli <Mohammed180726@feng.bu.edu.eg>
2024-11-24 20:08:24 +0200

    Add get it package for dependency injection.

---
+++ lib/core/services/get_it_service.dart
@@ -0,0 +1,14 @@
+import 'package:fakahany/core/services/firebase_auth_service.dart';
+import 'package:fakahany/features/auth/domain/repos/auth_repo.dart';
+import 'package:get_it/get_it.dart';
+
+import '../../features/auth/data/repos/auth_repo_impl.dart';
+
+final getIt = GetIt.instance;
+
+void setup() {
+  getIt.registerSingleton<FirebaseAuthService>(FirebaseAuthService());
+  getIt.registerSingleton<AuthRepo>(AuthRepoImpl(
+    firebaseAuthService: getIt<FirebaseAuthService>(),
+  ));
+}
--- lib/features/auth/presentation/views/signup_view.dart
+++ lib/features/auth/presentation/views/signup_view.dart
@@ -1,15 +1,27 @@
 import 'package:fakahany/core/widgets/custom_app_bar.dart';
+import 'package:fakahany/features/auth/presentation/signup_cubits/signup_cubit.dart';
 import 'package:fakahany/features/auth/presentation/views/widgets/signup_view_body.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+import '../../../../core/services/get_it_service.dart';
+import '../../domain/repos/auth_repo.dart';
+
 class SignupView extends StatelessWidget {
   const SignupView({super.key});
 
   static const routeName = 'signup';
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: buildAppBar(context, title: 'حساب جديد'),
-      body: SignupViewBody(),
+    return BlocProvider(
+      create: (context) => SignupCubit(
+        getIt.get<AuthRepo>(),
+      ),
+      child: Scaffold(
+        appBar: buildAppBar(context, title: 'حساب جديد'),
+        body: SignupViewBody(),
+      ),
     );
   }
 }
