c22692b828ad3d9e93d219ecf0b44e4d502828d5
MhammadAli <Mohammed180726@feng.bu.edu.eg>
2025-03-03 22:51:52 +0200

    Create custom bottom navigation bar.

--- lib/features/home/presentation/views/home_view.dart
+++ lib/features/home/presentation/views/home_view.dart
@@ -1,3 +1,4 @@
+import 'package:fakahany/features/home/presentation/views/widgets/custom_bottom_navigation_bar.dart';
 import 'package:fakahany/features/home/presentation/views/widgets/home_view_body.dart';
 import 'package:flutter/material.dart';
 
@@ -9,6 +10,7 @@ class HomeView extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const Scaffold(
+      bottomNavigationBar: CustomBottomNavigationBar(),
       body: SafeArea(child: HomeViewBody()),
     );
   }
