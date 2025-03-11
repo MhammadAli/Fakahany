c5d777aacc7e03201404397c295a078c5d1e576f
MhammadAli <Mohammed180726@feng.bu.edu.eg>
2025-03-11 21:48:53 +0200

    Create best selling view

--- lib/core/helper_functions/on_generate_routes.dart
+++ lib/core/helper_functions/on_generate_routes.dart
@@ -4,6 +4,7 @@ import 'package:flutter/material.dart';
 
 import '../../features/auth/presentation/views/signin_view.dart';
 import '../../features/auth/presentation/views/signup_view.dart';
+import '../../features/best_selling_fruits/presentation/views/best_selling_view.dart';
 import '../../features/home/presentation/views/home_view.dart';
 
 Route<dynamic> onGenerateRoute(RouteSettings settings) {
@@ -28,6 +29,11 @@ Route<dynamic> onGenerateRoute(RouteSettings settings) {
     case HomeView.routeName:
       return MaterialPageRoute(
         builder: (context) => const HomeView(),
+      );
+
+    case BestSellingView.routeName:
+      return MaterialPageRoute(
+        builder: (context) => const BestSellingView(),
       );
     default:
       return MaterialPageRoute(
